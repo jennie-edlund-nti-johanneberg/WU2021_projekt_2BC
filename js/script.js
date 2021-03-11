7e85c20373f606c9ce7ed93be47aac59cc9e8fba
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2021-03-11 17:26:28 +0100

    Utanför lektion v10

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 document.querySelector(".meny_knapp").addEventListener("click", showMeny)
 document.querySelector(".fade_layer").addEventListener("click", showMeny)
+/*  document.querySelector(".smooth_scroll").addEventListener("click", moreInfoScroll) */
 
 function showMeny() {
-    document.querySelector("nav").classList.toggle("visa")
-    document.querySelector(".fade_layer").classList.toggle("synlig")
+    document.querySelector("nav").classList.toggle("visa");
+    document.querySelector(".fade_layer").classList.toggle("synlig");
 }
+
+/* function moreInfoScroll() {
+    document.querySelector("#footer").scrollIntoView({ behavior: "smooth", block: "end"});
+} */
