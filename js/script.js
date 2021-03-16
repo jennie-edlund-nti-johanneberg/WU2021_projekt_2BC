4941ece2cca2d2fd4733572ea00bb809469fc48d
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2021-03-16 15:14:30 +0100

    Lektion v.11

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 document.querySelector(".meny_knapp").addEventListener("click", showMeny)
 document.querySelector(".fade_layer").addEventListener("click", showMeny)
-/*  document.querySelector(".smooth_scroll").addEventListener("click", moreInfoScroll) */
+document.addEventListener("scroll", handleScroll);
+document.querySelector(".to_top_knapp").addEventListener("click", scrollToTop);
 
 function showMeny() {
     document.querySelector("nav").classList.toggle("visa");
     document.querySelector(".fade_layer").classList.toggle("synlig");
 }
 
-/* function moreInfoScroll() {
-    document.querySelector("#footer").scrollIntoView({ behavior: "smooth", block: "end"});
-} */
+var scrollToTopBtn = document.querySelector(".to_top_knapp");
+function handleScroll() {
+    var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var GOLDEN_RATIO = 0.15;
+
+    if ((document.documentElement.scrollTop / scrollableHeight) > GOLDEN_RATIO) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+}
